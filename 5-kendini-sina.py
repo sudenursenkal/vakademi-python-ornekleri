6c8b0c9e78381188c70e1ebdfdee7271c2fe06d1
sudenursenkal <93382144+sudenursenkal@users.noreply.github.com>
2022-08-23 16:17:09 +0300

    5. kendini sina bolumunun ikinci adimi tamamlandi

--- 5-kendini-sina.py
+++ 5-kendini-sina.py
@@ -13,3 +13,15 @@ users = [
 for user in users:
     print(user[0], user[1])
 
+# 2. Kullanıcı adının ilk karakteri, soyadın ilk 3 karakteri
+# gözükecek şekilde kalan kısımları yıldızlarla doldurarak
+# bilgileri ekranda görüntüleyiniz.
+# H**** Yal*******
+
+for user in users:
+    print(
+        user[0][0] + ("*" * (len(user[0]) - 1)),
+        user[1][:3] + ("*" * (len(user[1]) - 3))
+        )
+# Ismin uzunluguna bakip gorunen harf sayisini cikartinca
+# istenen kadar yildiz kullanmis olduk.
