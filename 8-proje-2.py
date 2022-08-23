07a9b3dc5abf42861f6716d586f3c8b073a121dd
sudenursenkal <93382144+sudenursenkal@users.noreply.github.com>
2022-08-23 21:19:16 +0300

    sayi tahmin oyunu tamamlandi

---
+++ 8-proje-2.py
@@ -0,0 +1,15 @@
+# Tahmin Oyunu
+
+# 1 ile 10 Arasinda Bir Sayıyı Bilgisayar Tutuyor ve Biz Tahmin Etmeye Çalışıyoruz
+# Örnek Olarak Python 4 Sayısını Tuttu
+# Tahminizi Giriniz : Biz 7 girdik
+# Aşağı..
+# 3
+# Yukarı..
+# 4
+# Tebrikler :)
+
+from random import randint
+
+# bir ile on arasında bir sayıyı numberda tutuyoruz
+number = randint(1, 10)
